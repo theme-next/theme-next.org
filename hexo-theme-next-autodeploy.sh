fb91637b75c9b3cb1d45a8a15b847f2d958c702d
Ivan.Nginx <ivan.nginx@gmail.com>
2019-03-02 14:01:41 +0100

    Colored NexT & Modules.

--- hexo-theme-next-autodeploy.sh
+++ hexo-theme-next-autodeploy.sh
@@ -70,10 +70,10 @@ echo
 echo "=============================================================="
 echo " Cloning and Updating NexT theme & Adding needed modules..."
 echo "=============================================================="
-    echo "${blue}NexT{norm}"
+    echo "${blue}${bold}NexT${norm}"
     git -C themes/next pull || git clone https://github.com/theme-next/hexo-theme-next themes/next
-    echo
-    echo "jquery-lazyload"
+    echo -e
+    echo "${yellow}jquery-lazyload${norm}"
     git -C themes/next/source/lib/jquery_lazyload pull || git clone https://github.com/theme-next/theme-next-jquery-lazyload themes/next/source/lib/jquery_lazyload
 
 echo
