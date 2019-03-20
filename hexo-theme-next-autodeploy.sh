4baa73d0a32455e6d8bdc4eb65541757f930843e
ivan-nginx <ivan.nginx@gmail.com>
2019-03-20 13:39:02 +0300

    Fixed fancybox typo.

--- hexo-theme-next-autodeploy.sh
+++ hexo-theme-next-autodeploy.sh
@@ -86,7 +86,7 @@ echo "=============================================================="
 echo "${yellow}NexT:${norm} ${lcyan}`git -C themes/next pull || git clone https://github.com/theme-next/hexo-theme-next themes/next`${norm}"
 
 echo "${yellow}jquery-lazyload:${norm} ${lcyan}`git -C themes/next/source/lib/jquery_lazyload pull || git clone https://github.com/theme-next/theme-next-jquery-lazyload themes/next/source/lib/jquery_lazyload`${norm}"
-echo "${yellow}jquery-lazyload:${norm} ${lcyan}`git -C themes/next/source/lib/fancybox pull || git clone https://github.com/theme-next/theme-next-fancybox3 themes/next/source/lib/fancybox`${norm}"
+echo "${yellow}fancybox:${norm} ${lcyan}`git -C themes/next/source/lib/fancybox pull || git clone https://github.com/theme-next/theme-next-fancybox3 themes/next/source/lib/fancybox`${norm}"
 
 echo
 echo "=============================================================="
